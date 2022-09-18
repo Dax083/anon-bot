ec8af8cfa59fe88f774cb2dc4846712fc8c4c992
Vvslh project <96542899+Dax083@users.noreply.github.com>
2022-09-18 14:47:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5768016034:AAERRE7w4fgG0KSslRZld1I1dkGqTGDcyBU") # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kang_culiknew") # Isi dengan username kalian tanpa tanda @
-GROUP = os.environ.get("GROUP", "") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
-CHANNEL = os.environ.get("CHANNEL", "vvslh_pro") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
+TOKEN = os.environ.get("TOKEN", "") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "kang_culiknew") # jangan dirubah agar tidak error
+GROUP = os.environ.get("GROUP", "") # 
+CHANNEL = os.environ.get("CHANNEL", "vvslh_pro") # 
